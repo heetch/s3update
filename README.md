4d88079fce247589883dfe84a7441d3ad4542c83
Florian Bertholin <florian.bertholin@gmail.com>
2016-07-05 18:52:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # s3update
-Enable your Golang applications to self update with S3
+
+__Enable your Golang applications to self update with S3.__
+
+This package enables our internal tools to be updated when new commits to their master branch are pushed to Github.
+
+Latest binaries are hosted on S3 under a specific bucket along its current version. When ran locally, the binary will
+fetch the version and if its local version is older than the remote one, the new binary will get fetched and will exit,
+stating to the user that it got updated and need to be ran again.
+
+In our case, we're only shipping Linux and Darwin, targeting amd64 platform.
+
+Bucket will have the following structure:
+
+```
+mybucket/
+  mytool/
+	VERSION
+	mytool-linux-amd64
+	mytool-darwin-amd64
+```
+
+## Usage
+
+Updates are easier to deal with when done through a continuous integration platform. We're using CircleCI but the following
+excerpt can easilly be adapted to whichever solution being used.
+
+### CircleCI
+
+[xgo](https://github.com/karalabe/xgo) is being used to easily cross-compile code.
+
+Adding the following at the end of the build script will push the binaries and its version to S3.
+
+```sh
+xgo --targets="linux/amd64,darwin/amd64" -ldflags="-X main.Version=$CIRCLE_BUILD_NUM" .
+
+if [[ "$CIRCLE_BRANCH" = "master" ]]; then
+	aws s3 cp mytool-darwin-10.6-amd64 s3://mybucket/mytool/mytool-darwin-amd64 --acl authenticated-read
+	aws s3 cp mytool-linux-amd64 s3://mybucket/mytool/mytool-linux-amd64 --acl authenticated-read
+	echo -n $CIRCLE_BUILD_NUM > VERSION && aws s3 cp VERSION  s3://mybucket/mytool/VERSION --acl authenticated-read
+fi
+```
+
+### Example
+
+```go
+package main
+
+import (
+	"github.com/heetch/s3update"
+)
+
+var (
+	// This gets set during the compilation. See bellow
+	Version = ""
+)
+
+func main() {
+	s3update.AutoUpdate(s3update.Updater{
+		CurrentVersion: Version,
+		S3Bucket:       "mybucket",
+		S3Region:       "eu-west-1",
+		S3ReleaseKey:   "mytool/mytool-{{OS}}-{{ARCH}}",
+		S3VersionKey:   "mytool/VERSION",
+	})
+
+  ...
+}
+```
+
+Binary must be compiled with a flag specifying the new version:
+
+```sh
+go build -ldflags "-X main.Version=111" main.go
+```
+
+## Contributions
+
+Any contribution is welcomed!
+
+- Open an issue if you want to discuss bugs/features
+- Open Pull Requests if you want to contribute to the code
+
+## Copyright
+
+Copyright © 2016 Heetch
+
+See the [LICENSE](https://github.com/heetch/s3update/blob/master/LICENSE) (MIT) file for more details.
+
+
+
---
+++ s3update.go
@@ -0,0 +1,136 @@
+package s3update
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/kardianos/osext"
+)
+
+type Updater struct {
+	// CurrentVersion represents the current binary version.
+	// This is generally set at the compilation time with -ldflags "-X main.Version=42"
+	// See the README for additional information
+	CurrentVersion string
+	// S3Bucket represents the S3 bucket containing the different files used by s3update.
+	S3Bucket string
+	// S3Region represents the S3 region you want to work in.
+	S3Region string
+	// S3ReleaseKey represents the raw key on S3 to download new versions.
+	// The value can be something like `cli/releases/cli-{{OS}}-{{ARCH}}`
+	S3ReleaseKey string
+	// S3VersionKey represents the key on S3 to download the current version
+	S3VersionKey string
+}
+
+// validate ensures every required fields is correctly set. Otherwise and error is returned.
+func (u Updater) validate() error {
+	if u.CurrentVersion == "" {
+		return fmt.Errorf("no version set")
+	}
+
+	if u.S3Bucket == "" {
+		return fmt.Errorf("no bucket set")
+	}
+
+	if u.S3Region == "" {
+		return fmt.Errorf("no s3 region")
+	}
+
+	if u.S3ReleaseKey == "" {
+		return fmt.Errorf("no s3ReleaseKey set")
+	}
+
+	if u.S3VersionKey == "" {
+		return fmt.Errorf("no s3VersionKey set")
+	}
+
+	return nil
+}
+
+// AutoUpdate runs synchronously a verification to ensure the binary is up-to-date.
+// If a new version gets released, the download will happen automatically
+// It's possible to bypass this mechanism by setting the S3UPDATE_DISABLED environment variable.
+func AutoUpdate(u Updater) {
+	if os.Getenv("S3UPDATE_DISABLED") != "" {
+		fmt.Println("s3update: autoupdate disabled")
+		return
+	}
+
+	if err := u.validate(); err != nil {
+		fmt.Printf("s3update: %s - skipping auto update\n", err.Error())
+		return
+	}
+
+	if err := runAutoUpdate(u); err != nil {
+		fmt.Printf("s3update: ERROR: %s\n", err.Error())
+	}
+}
+
+// generateS3ReleaseKey dynamically builds the S3 key depending on the os and architecture.
+func generateS3ReleaseKey(path string) string {
+	path = strings.Replace(path, "{{OS}}", runtime.GOOS, -1)
+	path = strings.Replace(path, "{{ARCH}}", runtime.GOARCH, -1)
+
+	return path
+}
+
+func runAutoUpdate(u Updater) error {
+	localVersion, err := strconv.ParseInt(u.CurrentVersion, 10, 64)
+	if err != nil || localVersion == 0 {
+		return fmt.Errorf("invalid local version")
+	}
+
+	svc := s3.New(session.New(), &aws.Config{Region: aws.String(u.S3Region)})
+	resp, err := svc.GetObject(&s3.GetObjectInput{Bucket: aws.String(u.S3Bucket), Key: aws.String(u.S3VersionKey)})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	remoteVersion, err := strconv.ParseInt(string(b), 10, 64)
+	if err != nil || remoteVersion == 0 {
+		return fmt.Errorf("invalid remote version")
+	}
+
+	fmt.Printf("s3update: Local Version %d - Remote Version: %d\n", localVersion, remoteVersion)
+	if localVersion < remoteVersion {
+		fmt.Printf("s3update: version outdated ... \n")
+		s3Key := generateS3ReleaseKey(u.S3ReleaseKey)
+		resp, err := svc.GetObject(&s3.GetObjectInput{Bucket: aws.String(u.S3Bucket), Key: aws.String(s3Key)})
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		dir, err := osext.Executable()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("s3update: downloading new version to %s\n", dir)
+		if err := ioutil.WriteFile(dir, data, 0755); err != nil {
+			return err
+		}
+
+		fmt.Printf("s3update: updated with success to version %d\n", remoteVersion)
+		os.Exit(1)
+	}
+
+	return nil
+}
